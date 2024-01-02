495a642d8acceced07c14f443fd68169e52e07a7
MarinaGV93 <marinasgv@hotmail.com>
2024-01-02 14:55:06 -0300

    Trabalhando com Variáveis

---
+++ tipos_variaveis.js
@@ -0,0 +1,13 @@
+//String - Textos
+let nome = "Pikachu"
+let sexo = "F"
+ 
+//Number - numeros
+let nivel = 20
+let pontosDeVida = 45
+
+//Boolean - Variaveis logicos
+let selecionado = true
+
+//Numero que nao for fazer alteração matematica, o ideal é guardar como texto
+let tel = "(11)111111111"
---
+++ variaveis_e_constantes.js
@@ -0,0 +1,45 @@
+//Imprimir na tela
+console.log("Digite o nome do jogador")
+
+//Declarar variável
+let nome = "Marina"
+
+//Concatenar mensagem fixa + variavel
+console.log("Bem vindo " + nome)
+
+//Declarar uma constante (que não varia)
+const msg = "Pokemon: "
+
+console.log(msg + "você entrou em uma nova região")
+
+//Mudar variavel
+nome = "Mah"
+
+console.log("Agora o jogador se chama " + nome)
+
+
+let pokemon1 = "Pikachu"
+let pokemon2 = "Bulbasaur"
+let pokemon3 = "Charmander"
+let pokemon4 = "Squirtle"
+let pokemon5 = "Caterpie"
+const regiao = "Nessa região tem o: "
+
+console.log(regiao + 
+pokemon1 + ", " + 
+pokemon2 + ", " +
+pokemon3 + ", " +
+pokemon4 + " e " + 
+pokemon5 + ".")
+
+pokemon5 = "Butterfree"
+
+console.log(regiao + 
+pokemon1 + ", " + 
+pokemon2 + ", " +
+pokemon3 + ", " +
+pokemon4 + " e " + 
+pokemon5 + ".")
+
+console.clear()
+
